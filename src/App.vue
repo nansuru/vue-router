a4d6e138cb35f3fed1ed9db9b9e226ac59d9d320
DESKTOP-H3L3I4U\ficti <madebyrh@gmail.com>
2021-01-11 17:24:16 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div>
+      <router-link to="/" class="btn btn-primary mx-2"> Go to Top </router-link>
+      <router-link to="/jsx" class="btn btn-warning"> Go to JSX </router-link>
+    </div>
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  created() {
+    console.log("***** App Created! *****");
+  },
+  mounted() {
+    console.log("----- App Mounted! -----");
+  },
+  data() {
+    return {
+      title: "Router",
+    };
+  },
+};
+</script>
